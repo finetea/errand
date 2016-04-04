75f8c8116b9f7afaaca79c2ac987dc7e3849ffcb
finetea <finetea@spacex.dev.local>
2016-04-04 23:35:28 +0900

    initial upload for server skeleton

---
+++ server/apis/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from apis.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
---
+++ server/softcontract/urls.py
@@ -0,0 +1,46 @@
+"""softcontract URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+   https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+   1. Add an import:  from my_app import views
+   2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+   1. Add an import:  from other_app.views import Home
+   2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+   1. Import the include() function: from django.conf.urls import url, include
+   2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import patterns, url, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from django.contrib import admin
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+#router.register(r'groups', GroupViewSet)
+
+admin.autodiscover()
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browseable API.
+urlpatterns = patterns('',
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls',namespace='rest_framework')),
+    url(r'^admin/', include(admin.site.urls)),
+)
