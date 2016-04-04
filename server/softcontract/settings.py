dac38dab02e6ad52f944d71524079f84806d97c8
finetea <finetea@spacex.dev.local>
2016-04-04 23:51:50 +0900

    use mysql as main database

--- server/softcontract/settings.py
+++ server/softcontract/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'softcontract.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'softcontract',
+        'USER': 'scuser',
+        'PASSWORD': 'scpassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
